c6c3065e5c0f1265efd9b63fe5d7285f8022b0fd
Ondrej Kokes <ondrej.kokes@gmail.com>
2023-09-07 13:02:30 +0200

    nova 2023 data + oprava starych URL

--- main.py
+++ main.py
@@ -21,14 +21,15 @@ HEADER = [
 ]
 
 URLS = [
-    "https://opendata.praha.eu/dataset/86b98145-2d8a-46b9-8d24-cafb7c4ece20/resource/ac5664d7-caa8-4dce-8f75-906aea178ffd/download/d79d4145-bcf0-4bb3-b9de-92408e75ffb0-mhmp_dopravni_prestupky_2015.csv",
-    "https://opendata.praha.eu/dataset/df4b4faa-7a79-4b96-82f8-c855623ea3e0/resource/392c6bd4-daaa-4aa6-97d5-fc3e544f2f66/download/7736a486-36b3-47c1-8fc4-65ef1ffabf61-mhmp_dopravni_prestupky_2016.csv",
-    "https://opendata.praha.eu/dataset/d1c43e25-11d4-49ec-a4df-43c1821b2f88/resource/f2b80aff-7864-49c4-859c-893242bf6713/download/b1801060-b54f-4fbf-a9ae-215df2f6b3a6-mhmp_dopravni_prestupky_2017.csv",
-    "https://opendata.praha.eu/dataset/0d285576-1863-4725-9f81-f8490e066eb3/resource/fd3eecac-3594-45a8-a0ed-b8ca392bf7ca/download/1199d54b-4421-4d5d-bc53-8e23d4aeb1d5-mhmp_dopravni_prestupky_2018.csv",
+    "https://storage.golemio.cz/lkod/mhp/1e4da486-0229-44df-b3de-29a307a3e025/d79d4145-bcf0-4bb3-b9de-92408e75ffb0-mhmp_dopravni_prestupky_2015.csv",
+    "https://storage.golemio.cz/lkod/mhp/13d0d2b0-63e4-4f6a-adf3-12448300261e/7736a486-36b3-47c1-8fc4-65ef1ffabf61-mhmp_dopravni_prestupky_2016.csv",
+    "https://storage.golemio.cz/lkod/mhp/70c0712c-7252-45f7-88dd-ad63899f9eef/b1801060-b54f-4fbf-a9ae-215df2f6b3a6-mhmp_dopravni_prestupky_2017.csv",
+    "https://storage.golemio.cz/lkod/mhp/a8e902f2-325c-431c-ac84-1983c5bae130/1199d54b-4421-4d5d-bc53-8e23d4aeb1d5-mhmp_dopravni_prestupky_2018.csv",
     "https://storage.golemio.cz/ckan/mporga/MHMP_dopravni_prestupky_2019.zip",
     "https://storage.golemio.cz/ckan/mporga/MHMP_dopravni_prestupky_2020.zip",
     "https://storage.golemio.cz/ckan/mporga/MHMP_dopravni_prestupky_2021.zip",
     "https://storage.golemio.cz/ckan/mporga/MHMP_dopravni_prestupky_2022.zip",
+    "https://storage.golemio.cz/ckan/mporga/MHMP_dopravni_prestupky_2023.zip",
 ]
 
 table = "dopravni_prestupky"
@@ -80,5 +81,6 @@ if __name__ == "__main__":
         fw.write("set -eux\n")
         fw.write("psql < schema.sql\n")
         for url in URLS:
+            print(url)
             fn = url_as_local_file(url)
             fw.write(f"cat {fn} | psql -c 'COPY {table} FROM STDIN CSV HEADER'\n")
